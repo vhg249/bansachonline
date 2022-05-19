23f1f3748aafa71ce105c3bb28bf5ff17af01959
vhg249 <vhuongptit@gmail.com>
2022-05-19 22:40:11 +0700

    update

--- src/pages/admin/product/update.jsx
+++ src/pages/admin/product/update.jsx
@@ -2,171 +2,171 @@ import React, { useEffect, useState } from "react";
 import { Form, LoginWrapper } from "../../signup/style";
 import { Input } from "../../../shared/components/Input";
 import { Button } from "../../../shared/components/Button";
-import { Link, useNavigate,useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../../constant";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 
 export const UpdateBook = () => {
-    const [firstName, setFirstName] = useState("");
-    const [price, setPrice] = useState("");
-    const [sale, setSale] = useState("");
-    const [image, setImage] = useState("");
-    const [author, setAuthor] = useState("");
-    const [name, setName] = useState("");
-    const [quantity, setQuantity] = useState("");
-    const [description, setDescription] = useState("")
-    const [data, setData] = useState();
-    
+  const [firstName, setFirstName] = useState("");
+  const [price, setPrice] = useState("");
+  const [sale, setSale] = useState("");
+  const [image, setImage] = useState("");
+  const [author, setAuthor] = useState("");
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [description, setDescription] = useState("");
+  const [data, setData] = useState();
+
   const [category, setCategory] = useState("");
   const { id } = useParams();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const token = useSelector((state) => state.account.token);
-    const getBookById = () => {
-        axios.get(`${API_URL}/Book/getListBook?_id=${id}`)
-          .then(function (response) {
-            console.log(response);
-            setData(response.data.data.result[0]);
-            setAuthor(response.data.data.result[0].author);
-            setPrice(response.data.data.result[0].price);
-            setCategory(response.data.data.result[0].category[0]);
-            setPrice(response.data.data.result[0].price);
-            setQuantity(response.data.data.result[0].quantity);
-            setName(response.data.data.result[0].name);
-            setDescription(response.data.data.result[0].description);
-            setSale(response.data.data.result[0].sale);
-            setImage(response.data.data.result[0].image);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
-    const updateBook = async (body) => {
-        try {
-            const res = await axios.put(`${API_URL}/Book/update`, body, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + token,
-                },
-            })
-            return res.data.data;
-        }
-        catch (err) {
-            console.log(err);
-            toast.error(err.response.data.message)
-            return null;
-        }
-    };
-
-
-
-    //   const validate = () => {
-    //     if(email === "" ||category==="" || password === "" || confirmPassword === "" || phoneNumber === "" || address === "" || username === "" || firstName === "" || lastName === ""){
-    //       toast.error("Vui lòng điền đầy đủ thông tin");
-    //       return false;
-    //   }
-    //   else return true;
-    // }
-
-    const addBooks = async () => {
-        // if(validate()){
-        const res = await updateBook({
-            _id:id,
-            name: name,
-            author: author,
-            category: [category],
-            description: description,
-            image: image,
-            price: price,
-            quantity: quantity,
-            sale: sale
-        })
-        if (res) {
-            toast.success("Success");
-            navigate("/manager/product");
-        }
-        // }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.account.token);
+  const getBookById = () => {
+    axios
+      .get(`${API_URL}/Book/getListBook?_id=${id}`)
+      .then(function (response) {
+        console.log(response);
+        setData(response.data.data.result[0]);
+        setAuthor(response.data.data.result[0].author);
+        setPrice(response.data.data.result[0].price);
+        setCategory(response.data.data.result[0].category[0]);
+        setPrice(response.data.data.result[0].price);
+        setQuantity(response.data.data.result[0].quantity);
+        setName(response.data.data.result[0].name);
+        setDescription(response.data.data.result[0].description);
+        setSale(response.data.data.result[0].sale);
+        setImage(response.data.data.result[0].image);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const updateBook = async (body) => {
+    try {
+      const res = await axios.put(`${API_URL}/Book/update`, body, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+      return res.data.data;
+    } catch (err) {
+      console.log(err);
+      toast.error(err.response.data.message);
+      return null;
     }
-    useEffect(()=>{getBookById()},[]);
+  };
 
-    return (
-        <div className="container">
-            <LoginWrapper flexDirection={"column"} alignItems={"center"}>
-                <h1>Sửa sách</h1>
-                <Form>
-                    <Input
-                        label="Name"
-                        type="text"
-                        placeholder="Nhập tên"
-                        required={true}
-                        value={name}
-                        setValue={setName}
-                    />
-                    <Input
-                        label="Author"
-                        type="text"
-                        placeholder="Nhập tác giả"
-                        required={true}
-                        value={author}
-                        setValue={setAuthor}
-                    />
-                    <Input
-                        label="Category"
-                        type="text"
-                        placeholder="Nhập category"
-                        required={true}
-                        value={category}
-                        setValue={setCategory}
-                    />
-                    <Input
-                        label="Description"
-                        type="text"
-                        placeholder="Nhập miêu ta"
-                        required={true}
-                        value={description}
-                        setValue={setDescription}
-                    />
-                    <Input
-                        label="Image"
-                        type="string"
-                        placeholder="Nhập url"
-                        required={true}
-                        value={image}
-                        setValue={setImage}
-                    />
+  //   const validate = () => {
+  //     if(email === "" ||category==="" || password === "" || confirmPassword === "" || phoneNumber === "" || address === "" || username === "" || firstName === "" || lastName === ""){
+  //       toast.error("Vui lòng điền đầy đủ thông tin");
+  //       return false;
+  //   }
+  //   else return true;
+  // }
 
-                    <Input
-                        label="Price"
-                        type="number"
-                        placeholder="Nhập giá"
-                        required={true}
-                        value={price}
-                        setValue={setPrice}
-                    />
-                    <Input
-                        label="Quantity"
-                        type="number"
-                        placeholder="Nhập số lượng"
-                        required={true}
-                        value={quantity}
-                        setValue={setQuantity}
-                    />
+  const addBooks = async () => {
+    // if(validate()){
+    const res = await updateBook({
+      _id: id,
+      name: name,
+      author: author,
+      category: [category],
+      description: description,
+      image: image,
+      price: price,
+      quantity: quantity,
+      sale: sale,
+    });
+    if (res) {
+      toast.success("Success");
+      navigate("/manager/product");
+    }
+    // }
+  };
+  useEffect(() => {
+    getBookById();
+  }, []);
 
-                    <Input
-                        label="Sale"
-                        type="String"
-                        placeholder="Nhập sale"
-                        required={false}
-                        value={sale}
-                        setValue={setSale}
-                    />
+  return (
+    <div className="container">
+      <LoginWrapper flexDirection={"column"} alignItems={"center"}>
+        <h1>Sửa sách</h1>
+        <Form>
+          <Input
+            label="Name"
+            type="text"
+            placeholder="Nhập tên"
+            required={true}
+            value={name}
+            setValue={setName}
+          />
+          <Input
+            label="Author"
+            type="text"
+            placeholder="Nhập tác giả"
+            required={true}
+            value={author}
+            setValue={setAuthor}
+          />
+          <Input
+            label="Category"
+            type="text"
+            placeholder="Nhập category"
+            required={true}
+            value={category}
+            setValue={setCategory}
+          />
+          <Input
+            label="Description"
+            type="text"
+            placeholder="Nhập miêu ta"
+            required={true}
+            value={description}
+            setValue={setDescription}
+          />
+          <Input
+            label="Image"
+            type="string"
+            placeholder="Nhập url"
+            required={true}
+            value={image}
+            setValue={setImage}
+          />
 
-                    <Button onClick={addBooks}>Cập nhật sách</Button>
-                </Form>
-            </LoginWrapper>
-        </div>
-    );
+          <Input
+            label="Price"
+            type="number"
+            placeholder="Nhập giá"
+            required={true}
+            value={price}
+            setValue={setPrice}
+          />
+          <Input
+            label="Quantity"
+            type="number"
+            placeholder="Nhập số lượng"
+            required={true}
+            value={quantity}
+            setValue={setQuantity}
+          />
+
+          <Input
+            label="Sale"
+            type="String"
+            placeholder="Nhập sale"
+            required={false}
+            value={sale}
+            setValue={setSale}
+          />
+
+          <Button onClick={addBooks}>Cập nhật sách</Button>
+        </Form>
+      </LoginWrapper>
+    </div>
+  );
 };
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,11 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { API_URL } from "../constant";
 import { Input } from "../../shared/components/Input";
+import Flex from "../../shared/components/Flex";
+import { Button } from "../../shared/components/Button";
+import { toast } from "react-toastify";
 
 const ProfileWrapper = styled(VoucherWrapper)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 100px;
 `;
 
 export const Profile = () => {
@@ -17,9 +21,15 @@ export const Profile = () => {
   const isLogin = useSelector((state) => state.account.isLogin);
   const token = useSelector((state) => state.account.token);
   const dispatch = useDispatch();
+  const [isEdit, setIsEdit] = useState(false);
+  const [firstName, setFirstName] = useState("Nguyen");
+  const [lastName, setLastName] = useState("Huong");
+  const [email, setEmail] = useState("bbb@gmail.com");
+  const [phoneNumber, setPhoneNumber] = useState("0123456789");
+  const [address, setAddress] = useState("HaDong");
 
   useEffect(() => {
-      getMeApi();
+    getMeApi();
   }, []);
 
   const getMeApi = async () => {
@@ -38,6 +48,23 @@ export const Profile = () => {
     }
   };
 
+  const updateProfile = async (body) => {
+    try {
+      const res = await axios.put(`${API_URL}/Book/update`, body, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+      return res.data.data;
+    } catch (err) {
+      console.log(err);
+      toast.error(err.response.data.message);
+      return null;
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -46,40 +73,57 @@ export const Profile = () => {
           <Input
             label="Tên"
             type="text"
-            disabled={true}
-            value={myProfile ? myProfile.firstName : ""}
+            disabled={!isEdit}
+            value={!isEdit && myProfile ? myProfile.firstName : firstName}
+            setValue={setFirstName}
           />
           <Input
             label="Họ"
             type="text"
-            disabled={true}
-            value={myProfile ? myProfile.lastName : ""}
+            disabled={!isEdit}
+            value={!isEdit && myProfile ? myProfile.lastName : lastName}
+            setValue={setLastName}
           />
           <Input
             label="Email"
             type="email"
-            disabled={true}
-            value={myProfile ? myProfile.email : ""}
+            disabled={!isEdit}
+            value={!isEdit && myProfile ? myProfile.email : email}
+            setValue={setEmail}
           />
 
           <Input
             label="Địa chỉ"
             type="text"
-            disabled={true}
-            value={myProfile ? myProfile.address : ""}
+            disabled={!isEdit}
+            value={!isEdit && myProfile ? myProfile.address : address}
+            setValue={setAddress}
           />
           <Input
             label="Số điện thoại"
             type="text"
-            disabled={true}
-            value={myProfile ? myProfile.phonenumber : ""}
+            disabled={!isEdit}
+            value={!isEdit && myProfile ? myProfile.phoneNumber : phoneNumber}
+            setValue={setPhoneNumber}
           />
           <Input
             label="Mật khẩu"
             type="password"
-            disabled={true}
-            value={myProfile ? myProfile.password : ""}
+            disabled={!isEdit}
+            value={"1234567890"}
           />
+          <Flex gap={"20px"} justifyContent="center">
+            {isEdit ? (
+              <Button onClick={() => setIsEdit(true)}>Cập nhật</Button>
+            ) : (
+              <>
+                <Button onClick={() => setIsEdit(true)}>
+                  Cập nhật thông tin
+                </Button>
+                <Button>Đổi mật khẩu</Button>
+              </>
+            )}
+          </Flex>
         </ProfileWrapper>
       </div>
     </>
